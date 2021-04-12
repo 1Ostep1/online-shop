69ac1072aad76fba7f85a322337867ca589d6ce6
Ostep <ostep1@icloud.com>
2021-04-12 17:00:17 +0600

    Updates

--- src/main/java/com/example/greenshop/Controllers/WomenProductController.java
+++ src/main/java/com/example/greenshop/Controllers/WomenProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @Controller
 public class WomenProductController {
 
